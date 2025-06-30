0659fa06bf5b0f83559aca26a9c9bd91f90c191a
Marco Antoni <marco.antoni910@gmail.com>
2025-06-30 10:30:27 -0300

    Banco de dados

--- programacaoparainternet.sql
+++ programacaoparainternet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/06/2025 às 12:34
+-- Tempo de geração: 30/06/2025 às 15:30
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `cidades`
+--
+
+CREATE TABLE `cidades` (
+  `id` int(11) NOT NULL,
+  `cidade` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cidades`
+--
+
+INSERT INTO `cidades` (`id`, `cidade`) VALUES
+(1, 'Rolante'),
+(2, 'Igrejinhja'),
+(3, 'Taquara'),
+(4, 'Três Coroas'),
+(5, 'Parobé'),
+(6, 'Riozinho');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `usuarios`
 --
 
@@ -34,19 +57,30 @@ CREATE TABLE `usuarios` (
   `telefone` varchar(45) NOT NULL,
   `cpf` varchar(45) NOT NULL,
   `nascimento` date NOT NULL,
-  `senha` varchar(45) NOT NULL
+  `senha` varchar(45) NOT NULL,
+  `id_cidade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `telefone`, `cpf`, `nascimento`, `senha`) VALUES
-(1, 'MARCO', 'marco.antoni@rolante.ifrs.edu.br', '55999123537', '123.356.789-91', '2025-06-02', '1234');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `telefone`, `cpf`, `nascimento`, `senha`, `id_cidade`) VALUES
+(3, 'MARCO ANTONI', 'marco.antoni910@gmail.com', '55991137587', '025.616.720-69', '2025-06-09', '1234', 0),
+(4, 'MARCO ANTONI', 'marco.antoni910@gmail.com', '55997111593', '025.616.720-69', '2025-06-09', '1234', 0),
+(5, 'Gabriel', 'gabriel@gmail.com', '51 9 9123 4556', '098-765-432-12', '2008-02-09', '1234', 0),
+(6, 'Eduarda', 'duda@gmail.com', '51 9 9123 4556', '123.456.789-11', '2025-06-09', '1234', 0),
+(7, 'Kléber', 'kleber@gmail.com', '9843274238', '098-765-432-12', '2006-11-06', '1234', 1);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `cidades`
+--
+ALTER TABLE `cidades`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `usuarios`
@@ -59,10 +93,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `cidades`
+--
+ALTER TABLE `cidades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
